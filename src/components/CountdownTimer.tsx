192d6a752dabe88c7453b2653a3b56bb8ffd5090
GnussonNet <admin@gnusson.net>
2023-02-03 22:09:19 +0100

    Improved functionallity

--- src/components/CountdownTimer.tsx
+++ src/components/CountdownTimer.tsx
@@ -1,15 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useCountdown } from '../hooks/useCountdown';
 import { Text, TouchableOpacity, View } from 'react-native';
-
-const ExpiredNotice = () => {
-  return (
-    <View className='flex items-center'>
-      <Text className='text-white text-xl font-bold'>Time expired!</Text>
-      <Text className='text-white text-lg'>Please select a future date and time.</Text>
-    </View>
-  );
-};
+import dayjs from 'dayjs';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
 
 const ShowCounter = ({ minutes, seconds }: {
   minutes: number,
@@ -17,37 +10,74 @@ const ShowCounter = ({ minutes, seconds }: {
 }) => {
   return (
     <View>
-      <View className='flex flex-row justify-center gap-2'>
-        <Text className='text-white text-7xl font-extralight text-right w-24'>{minutes.toString().padStart(2, '0')}</Text>
-        <Text className='text-white text-7xl font-extralight bottom-2'>:</Text>
-        <Text className='text-white text-7xl font-extralight text-left w-24'>{seconds.toString().padStart(2, '0')}</Text>
+      <View className='flex flex-row ml-auto mr-auto bg-gray-800 rounded-full py-2 px-4 items-center'>
+        <Text className='text-gray-200 font-semibold mr-2'>Pomodoros</Text>
+        <View className='w-3 h-3 bg-gray-700 rounded-full mr-1'></View>
+        <View className='w-3 h-3 bg-gray-700 rounded-full mr-1'></View>
+        <View className='w-3 h-3 bg-gray-700 rounded-full mr-1'></View>
+        <View className='w-3 h-3 bg-gray-700 rounded-full'></View>
       </View>
-      <TouchableOpacity
-        className="mt-8"
-      >
-        <View className="bg-green-500 p-3 rounded-md">
-          <Text className="text-center font-semibold text-gray-900">
-            Give Up
-          </Text>
-        </View>
-      </TouchableOpacity>
+      <View className='flex flex-row justify-center gap-2 mt-2'>
+        <Text className='text-white text-8xl font-extralight text-right w-28'>{minutes.toString().padStart(2, '0')}</Text>
+        <Text className='text-white text-8xl font-extralight bottom-2'>:</Text>
+        <Text className='text-white text-8xl font-extralight text-left w-28'>{seconds.toString().padStart(2, '0')}</Text>
+      </View>
     </View>
   );
 };
 
-const CountdownTimer = ({ targetDate }: { targetDate: Date }) => {
+const CountdownTimer = () => {
+  const { getItem, setItem } = useAsyncStorage('@pomodoro_park_data');
+  const [targetDate, setTargetDate] = useState<string>("");
   const [minutes, seconds] = useCountdown(targetDate);
 
-  if (minutes + seconds <= 0) {
-    return <ExpiredNotice />;
-  } else {
-    return (
+  const readItemFromStorage = async () => {
+    const item = await getItem();
+    if (item)
+      setTargetDate(item);
+  };
+
+  const writeItemToStorage = async (newValue: string) => {
+    await setItem(newValue);
+    setTargetDate(newValue)
+  };
+
+  useEffect(() => {
+    readItemFromStorage();
+  }, []);
+
+  return (
+    <View>
       <ShowCounter
         minutes={minutes}
         seconds={seconds}
       />
-    );
-  }
+      {minutes + seconds <= 0 ? (
+
+        <TouchableOpacity
+          className="mt-12"
+          onPress={() => writeItemToStorage(dayjs().add(25, 'minutes').add(1, "second").toString())}
+        >
+          <View className="bg-green-500 p-3 rounded-md hover:bg-slate-300 active:bg-slate-500">
+            <Text className="text-center font-semibold text-gray-900">
+              Focus
+            </Text>
+          </View>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          className="mt-12"
+          onPress={() => writeItemToStorage("")}
+        >
+          <View className="bg-red-500 p-3 rounded-md">
+            <Text className="text-center font-semibold text-gray-900">
+              Give up
+            </Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    </View>
+  )
 };
 
 export default CountdownTimer;
--- src/hooks/useCountdown.ts
+++ src/hooks/useCountdown.ts
@@ -1,23 +1,32 @@
+import dayjs from 'dayjs';
 import { useEffect, useState, useRef } from 'react';
 
-const useCountdown = (targetDate: Date) => {
-  const countDownDate = new Date(targetDate).getTime();
+const useCountdown = (targetDate: string) => {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const intervalRef = useRef<number | undefined>(undefined);
+  const intervalRef = useRef<number>();
 
   useEffect(() => {
     const updateCountdown = () => {
-      const countDown = countDownDate - new Date().getTime();
-      setMinutes(Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60)));
-      setSeconds(Math.floor((countDown % (1000 * 60)) / 1000));
+      if (!targetDate) {
+        setMinutes(0);
+        setSeconds(0);
+        return
+      }
+      const countDown = dayjs(targetDate).diff(dayjs());
+      setMinutes(dayjs(countDown).get("minutes"));
+      setSeconds(dayjs(countDown).get("seconds"));
+      if (countDown <= 0) {
+        intervalRef.current && clearInterval(intervalRef.current);
+        return;
+      }
     };
 
     updateCountdown();
     intervalRef.current = setInterval(updateCountdown, 1000);
 
     return () => { intervalRef.current && clearInterval(intervalRef.current) };
-  }, [countDownDate]);
+  }, [targetDate]);
 
   return [minutes, seconds];
 };
