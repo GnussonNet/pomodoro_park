f54a2be1e07a47aca9676e38408196bc6fd845b9
GnussonNet <admin@gnusson.net>
2023-02-08 14:35:46 +0100

    Added Logout button

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,20 +1,22 @@
-import {Text, TouchableOpacity, View} from 'react-native';
+import {TouchableOpacity, View} from 'react-native';
 import React from 'react';
 import CountdownTimer from '../components/CountdownTimer';
 import auth from '@react-native-firebase/auth';
+import {LogOut} from 'lucide-react-native';
 
 const Home = () => {
   return (
     <View className="bg-white dark:bg-gray-900 px-8 py-4 h-full">
+      <TouchableOpacity
+        className="flex flex-row gap-2 absolute top-4 right-8"
+        onPress={() =>
+          auth()
+            .signOut()
+            .then(() => console.log('User signed out!'))
+        }>
+        <LogOut className="text-white" />
+      </TouchableOpacity>
       <View className="mt-auto">
-        <TouchableOpacity
-          onPress={() =>
-            auth()
-              .signOut()
-              .then(() => console.log('User signed out!'))
-          }>
-          <Text>Sign Out</Text>
-        </TouchableOpacity>
         <CountdownTimer />
       </View>
     </View>
